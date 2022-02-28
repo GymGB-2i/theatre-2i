485a060500ae274d30e7329c8ce81ccef6a119c5
Jonathan Bittner <bittner.jf@gmail.com>
2022-02-28 19:08:19 +0100

    change text import

--- pages/imprint.js
+++ pages/imprint.js
@@ -3,7 +3,7 @@ import Footer from "../components/footer.js";
 import axios from "axios";
 import Head from "next/head";
 const qs = require('qs')
-import Text from "../components/Text.js";
+import Text from "../components/text.js";
 
 const Home = ({ strapiData, error, headerData, footerData }) => {
     if ( error ) {

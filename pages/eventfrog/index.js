1d25f2555287b301d88e778357d26829394a3572
Jonathan Bittner <bittner.jf@gmail.com>
2022-03-14 14:35:32 +0100

    change iframe height

--- pages/eventfrog/index.js
+++ pages/eventfrog/index.js
@@ -6,7 +6,7 @@ export default function Eventfrog({ headerData }) {
     return (
         <main>
             <Header headerData={headerData}/>
-            <Script src="https://embed.eventfrog.ch/api/scripts/embed/create-event.js"></Script><iframe width="100%" height="100%" src="https://embed.eventfrog.ch/de/cockpitv3/create-event-step1?key=866336a0-5740-495b-9467-7e9e955a8359&color=3B82F6"></iframe>
+            <Script src="https://embed.eventfrog.ch/api/scripts/embed/create-event.js"></Script><iframe width="100%" height="100vh" src="https://embed.eventfrog.ch/de/cockpitv3/create-event-step1?key=866336a0-5740-495b-9467-7e9e955a8359&color=3B82F6"></iframe>
         </main>
     )
 }

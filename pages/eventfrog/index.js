3e775f6f1e3fdeaae6ed4a526f91417c9ee8e354
Jonathan Bittner <bittner.jf@gmail.com>
2022-03-14 14:27:24 +0100

    change to nextjs/script tag

--- pages/eventfrog/index.js
+++ pages/eventfrog/index.js
@@ -1,11 +1,12 @@
 import Header from "../../components/header"
 import axios from "axios"
+import Script from "next/script"
 
 export default function Eventfrog({ headerData }) {
     return (
         <main>
             <Header headerData={headerData}/>
-            <script src="https://embed.eventfrog.ch/api/scripts/embed/overview.js"></script><iframe width="100%" height="100%" src="https://embed.eventfrog.ch/de/events.html?key=866336a0-5740-495b-9467-7e9e955a8359&color=3B82F6&showSearch=true&excludeOrgs=false"></iframe>
+            <Script src="https://embed.eventfrog.ch/api/scripts/embed/overview.js"></Script><iframe width="100%" height="100%" src="https://embed.eventfrog.ch/de/events.html?key=866336a0-5740-495b-9467-7e9e955a8359&color=3B82F6&showSearch=true&excludeOrgs=false"></iframe>
         </main>
     )
 }
